b2c19d19d49be15b4e11e7e517b429a8698bb3f7
Torbjørn Vatn <torbjorn@vatn.org>
2010-04-13 20:20:19 +0200

    Cleaned up the javascript

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
     $("#datepicker").datepicker();
+
+    $('input[title!=""]').hint();
+
+    $("#plus").bind("keyup", updatePlusDate);
+    $("#minus").bind("keyup", updateMinusDate);
+
+    function updatePlusDate(event) {
+        updateDate(event, "#plus");
+    }
+
+    function updateMinusDate(event) {
+        updateDate(event, "#minus");
+    }
+
+    function updateDate(event, signId) {
+        var sign = (signId == "#plus") ? "" : "-";
+        if (validKeyup(event)) {
+            $.post(calculateAction(), {'date': $("#datepicker").val(), 'extraDays': sign+$(signId).val()}, function(date) {
+                $(signId+"_date").text(date);
+            }, "text");
+        }
+    }
+
+    function validKeyup(event) {
+        if (event.which > 47 &&  event.which < 58 || event.which == 8) {
+            return true;
+        } else {
+            return false
+        }
+    }
 });
 
-$(function(){
-    // find all the input elements with title attributes
-    $('input[title!=""]').hint();
-});
